aae4501470f1043175666a96712bb52367e2ab15
swimmingcircle119 <lampmaa22@gmail.com>
2023-06-26 21:36:20 -0700

    website update all

--- scripts/main.js
+++ scripts/main.js
@@ -12,9 +12,15 @@ myImages.forEach((image) => {
   };
 });
 
-function soundPOV(){
+function soundPOV() {
   var sound = new Audio('./sounds/collapse.wav');
-  sound.volume = 0.05; 
+  sound.volume = 0.05;
+  sound.play();
+};
+
+function soundAniket() {
+  var sound = new Audio('./sounds/aniket.wav');
+  sound.volume = 0.8;
   sound.play();
 };
 
@@ -23,69 +29,47 @@ function playSound() {
   audio.play();
 }
 
+
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = 'max-content';
-    } 
+    }
   });
 }
 
+// // Wait for the document to load
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Get references to the button and image container
+//   var showImageBtn = document.getElementById("showImageBtn");
+//   var imageContainer = document.getElementById("imageContainer");
 
-// const Confettiful = function(el) {
-//   this.el = el;
-//   this.containerEl = null;
-  
-//   this.confettiFrequency = 3;
-//   this.confettiColors = ['#EF2964', '#00C09D', '#2D87B0', '#48485E','#EFFF1D'];
-//   this.confettiAnimations = ['slow', 'medium', 'fast'];
-  
-//   this._setupElements();
-//   this._renderConfetti();
-// };
+//   // Button click event handler
+//   showImageBtn.addEventListener("click", function () {
+//     // Create an image element
+//     var image = document.createElement("img");
 
-// Confettiful.prototype._setupElements = function() {
-//   const containerEl = document.createElement('div');
-//   const elPosition = this.el.style.position;
-  
-//   if (elPosition !== 'relative' || elPosition !== 'absolute') {
-//     this.el.style.position = 'relative';
-//   }
-  
-//   containerEl.classList.add('confetti-container');
-  
-//   this.el.appendChild(containerEl);
-  
-//   this.containerEl = containerEl;
-// };
+//     // Set the image source
+//     image.src = "images/aniket.png"; // Replace with the actual image path
 
-// Confettiful.prototype._renderConfetti = function() {
-//   this.confettiInterval = setInterval(() => {
-//     const confettiEl = document.createElement('div');
-//     const confettiSize = (Math.floor(Math.random() * 3) + 7) + 'px';
-//     const confettiBackground = this.confettiColors[Math.floor(Math.random() * this.confettiColors.length)];
-//     const confettiLeft = (Math.floor(Math.random() * this.el.offsetWidth)) + 'px';
-//     const confettiAnimation = this.confettiAnimations[Math.floor(Math.random() * this.confettiAnimations.length)];
-    
-//     confettiEl.classList.add('confetti', 'confetti--animation-' + confettiAnimation);
-//     confettiEl.style.left = confettiLeft;
-//     confettiEl.style.width = confettiSize;
-//     confettiEl.style.height = confettiSize;
-//     confettiEl.style.backgroundColor = confettiBackground;
-    
-//     confettiEl.removeTimeout = setTimeout(function() {
-//       confettiEl.parentNode.removeChild(confettiEl);
-//     }, 3000);
-    
-//     this.containerEl.appendChild(confettiEl);
-//   }, 25);
-// };
+//     // Add the image to the container
+//     imageContainer.appendChild(image);
+//   });
+// });
 
-// window.confettiful = new Confettiful(document.querySelector('.js-container'));
+function replaceElement() {
+  var image = document.createElement('img');
+  image.src = "images/aniket.png";
+  var oldElement = document.getElementById('checkmark-circle');
+  console.log(oldElement);
+  oldElement.replaceWith(image);
+}
+
